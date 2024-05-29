a377c090f2c7aac9d034295fb07d9e4f66f40e7c
Ankuristic <63995112+Ankuristic@users.noreply.github.com>
2024-05-30 02:02:16 +0530

    checkbox added

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,22 +1,16 @@
-// import { Link } from "react-router-dom";
-import React from 'react'
-// import Navbar from '../components/Navbar'
-import './home.css'
+import React, { useState } from 'react';
+import './home.css';
 
-import image1 from "../Images/Electronics/image 1.jpg"
-import image2 from "../Images/Electronics/imge 2.jpg"
-import image3 from "../Images/Electronics/image 3.jpg"
-import image4 from "../Images/Electronics/image 4.jpg"
-import image5 from "../Images/jewelly/image 1.jpg"
-import image6 from "../Images/jewelly/image 2.jpg"
-import image7 from "../Images/jewelly/imaage 3.jpg"
-import image8 from "../Images/women/image 2.jpg"
-import image9 from "../Images/jewelly/image 4.jpg"
-import image11 from "../Images/men/image 1.jpg"
-
-
-
-
+import image1 from "../Images/Electronics/image 1.jpg";
+import image2 from "../Images/Electronics/imge 2.jpg";
+import image3 from "../Images/Electronics/image 3.jpg";
+import image4 from "../Images/Electronics/image 4.jpg";
+import image5 from "../Images/jewelly/image 1.jpg";
+import image6 from "../Images/jewelly/image 2.jpg";
+import image7 from "../Images/jewelly/imaage 3.jpg";
+import image8 from "../Images/women/image 2.jpg";
+import image9 from "../Images/jewelly/image 4.jpg";
+import image11 from "../Images/men/image 1.jpg";
 
 const menu = [
   { id: '1', images: image1 , description: "loreum" },
@@ -29,50 +23,59 @@ const menu = [
   { id: '8', images: image8, description: "loreum" },
   { id: '9', images: image9, description: "loreum" },
   { id: '11', images: image11, description: "loreum" },
-
 ];
 
+export const Home = () => {
+  const [clothings, setClothings] = useState([])
 
- export const Home = () => {
+  const handleChange =(e)=>{
+    console.log("handle change working");
+    const value = e.target.value;
+    const checked = e.target.checked;
+    console.log(value,checked);
+    if(checked){
+      setClothings([
+
+        ...clothings, value
+
+      ])
+    
+    }
+  }
   return (
     <div className="home">
-    <div className='search-form'>
-    <input type ="text"  placeholder="search by Name"/>
-     </div>
-     <div className='filter'>
-      <h1> Filter </h1>
-      <h2> Category </h2>
-<input type="checkbox" id="clothing" />
-  <label for="vehicle1"> Men's clothing</label>
-  <input type="checkbox" id="" />
-  <label for="vehicle2"> women's clothing</label>
-  <input type="checkbox" />
-  <label for="vehicle3"> clothing</label>
-  <input type="checkbox" />
-  <label for="vehicle3"> eletronis</label>
-
-
-     </div>
-     {/* <div className="menu-items">
-                {menu.map(item => (
-                    <div key={item.id} className="menu-item">
-                        <img src={item.images} alt={item.description} />
-                        <p>{item.description}</p>
-                    </div>
-                ))}
-            </div> */}
-
-
-            <div className="menu-items">
-    {menu.map((item,index) => (
-        <div key={index} className="menu-item">
-            <img src={item.images} alt={item.description} />
-            {/* <p>{in}</p> */}
+      <div className='search-form'>
+        <input type="text" placeholder="Search by Name" />
+      </div>
+      <div className='filter'>
+        <h1>Filter</h1>
+        <h2>Category</h2>
+        <div>
+          <input type="checkbox" name ="clothing" value="mens-clothing" onChange={handleChange} />
+          <label htmlFor="mens-clothing">Men's clothing</label>
         </div>
-    ))}
-</div>
-
-
+        <div>
+          <input type="checkbox"  name ="clothing" value="womens-clothing"  onChange={handleChange}/>
+          <label htmlFor="womens-clothing">Women's clothing</label>
+        </div>
+        <div>
+          <input type="checkbox" name ="clothing" value="jewellery" onChange={handleChange} />
+          <label htmlFor="jewellery">Jewellery</label>
+        </div>
+        <div>
+          <input type="checkbox" name ="clothing" value="electronics" onChange={handleChange} />
+          <label htmlFor="electronics">Electronics</label>
+        </div>
+      </div>
+      
+      <div className="menu-items">
+        {menu.map((item) => (
+          <div key={item.id} className="menu-item">
+            <img src={item.images} alt={item.description} />
+            <p>{item.description}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
